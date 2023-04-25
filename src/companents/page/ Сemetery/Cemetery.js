923285e614487941a6baa67ed29772c504a12486
rolanusonov <rolanusonov031209@gmail.com>
2023-04-26 00:31:17 +0900

    finish+

--- src/companents/page/ Сemetery/Cemetery.js
+++ src/companents/page/ Сemetery/Cemetery.js
@@ -5,15 +5,14 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./styles.css";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
-import img9 from "../../imgHome/IMG_20230413_110801.jpg";
-import img1 from "../../imgHome/IMG_20230413_105839.jpg";
-import img2 from "../../imgHome/IMG_20230413_105736.jpg";
-import img3 from "../../imgHome/IMG_20230413_105851.jpg";
-import img4 from "../../imgHome/IMG_20230413_105923.jpg";
-import img5 from "../../imgHome/IMG_20230413_105959.jpg";
-import img6 from "../../imgHome/IMG_20230413_110113.jpg";
-import img7 from "../../imgHome/IMG_20230413_110521.jpg";
-import img8 from "../../imgHome/IMG_20230413_110552.jpg";
+ import img1 from "../../imgCemetry/IMG_20230425_154206.jpg";
+import img2 from "../../imgCemetry/IMG_20230425_153958.jpg";
+import img3 from "../../imgCemetry/IMG_20230425_154003.jpg";
+import img4 from "../../imgCemetry/IMG_20230425_154016.jpg";
+import img5 from "../../imgCemetry/IMG_20230425_154023.jpg";
+import img6 from "../../imgCemetry/IMG_20230425_154026.jpg";
+import img7 from "../../imgCemetry/IMG_20230425_154112.jpg";
+import img8 from "../../imgCemetry/IMG_20230425_154118.jpg";
 
 const Cemetery = () => {
     return (
@@ -30,7 +29,7 @@ const Cemetery = () => {
                              keyboard={true}
                              modules={[Navigation, Pagination, Mousewheel, Keyboard]}
                              className="mySwiper"
-                         > <SwiperSlide><img src={img9} alt=""/></SwiperSlide>
+                         >
                              <SwiperSlide><img src={img1} alt=""/></SwiperSlide>
                              <SwiperSlide><img src={img2} alt=""/></SwiperSlide>
                              <SwiperSlide><img src={img3} alt=""/></SwiperSlide>
